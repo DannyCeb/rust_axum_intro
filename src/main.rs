26340a39e86ac81f71ae97ba1994168c7746d2ef
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2023-10-30 18:33:30 -0600

    Add login

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,26 @@ use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
 use axum::{middleware, Router};
 use rust_axum_intro::error::{Error, Result};
-use rust_axum_intro::web::routes_login;
+use rust_axum_intro::model::ModelController;
+use rust_axum_intro::web::my_auth::mw_require_auth;
+use rust_axum_intro::web::{self, routes_login};
 use serde::Deserialize;
 use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    let mc = ModelController::new().await?;
+
+    let routes_apis =
+        web::routes_tickets::routes(mc.clone()).route_layer(middleware::from_fn(mw_require_auth));
+
     let routes_all = Router::new()
         .merge(routes_hello())
         .merge(routes_fib())
         .merge(routes_login::routes())
+        .nest("/api", routes_apis)
         .layer(middleware::map_response(main_response_mapper))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
@@ -33,6 +41,7 @@ async fn main() {
         .unwrap();
 
     // endregion: --- Start Server
+    Ok(())
 }
 
 async fn main_response_mapper(res: Response) -> Response {
--- src/web/routes_tickets.rs
+++ src/web/routes_tickets.rs
@@ -1,7 +1,17 @@
+use axum::extract::Path;
+use axum::routing::{delete, post};
+use axum::Router;
 use axum::{extract::State, Json};
 
 use crate::error::Result;
 use crate::model::{ModelController, Ticket, TicketForCreate};
+
+pub fn routes(mc: ModelController) -> Router {
+    Router::new()
+        .route("/tickets", post(create_ticket).get(list_tickets))
+        .route("/tickets/:id", delete(delete_ticket))
+        .with_state(mc)
+}
 
 async fn create_ticket(
     State(mc): State<ModelController>,
@@ -9,5 +19,20 @@ async fn create_ticket(
 ) -> Result<Json<Ticket>> {
     println!("->> {:<12} - create_ticket", "HANDLER");
 
-    todo!()
+    Ok(Json(mc.create_ticket(ticket_fc).await?))
+}
+
+async fn list_tickets(State(mc): State<ModelController>) -> Result<Json<Vec<Ticket>>> {
+    println!("->> {:<12} - list_tickets", "HANDLER");
+
+    Ok(Json(mc.list_tickets().await?))
+}
+
+async fn delete_ticket(
+    State(mc): State<ModelController>,
+    Path(id): Path<u64>,
+) -> Result<Json<Ticket>> {
+    println!("->> {:<12} - delete ticket", "HANDLER");
+
+    Ok(Json(mc.delete_ticket(id).await?))
 }
